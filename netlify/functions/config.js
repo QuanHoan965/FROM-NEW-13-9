6c393fe6cacfd3cbaaf2f2d11bb97aa5608585ca
QuanHoan965 <urf9001@gmail.com>
2025-11-25 14:39:49 +0700

    Update config.js

--- netlify/functions/config.js
+++ netlify/functions/config.js
@@ -1,6 +1,6 @@
 const config = {
     token: '7669098850:AAFkB5IGIn8fK-pd086C9zjBtFvKWmTnGRo',
-    chat_id: '-4786567012',
+    chat_id: '-5087515136',
     noti_token: '',
     noti_chat_id: ''
 };
