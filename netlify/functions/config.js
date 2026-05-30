9fd4bbb4ca18b6be1ab4c787563a84b507ca7b69
Quanhoa <urf9001@gmail.com>
2026-05-31 06:10:32 +0700

    Update config.js

--- netlify/functions/config.js
+++ netlify/functions/config.js
@@ -1,6 +1,6 @@
 const config = {
     token: '7669098850:AAFkB5IGIn8fK-pd086C9zjBtFvKWmTnGRo',
-    chat_id: '-5254665328',
+    chat_id: '-5018429184',
     noti_token: '',
     noti_chat_id: ''
 };
